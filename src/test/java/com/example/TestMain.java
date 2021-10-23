4a85c2b7ba955eb2a11faacd1031c88e56e8e074
aravindpenmetsa <coolaravindster@gmail.com>
2021-10-23 20:24:28 +0530

    Add files via upload
    Initial checkin

---
+++ src/test/java/com/example/TestMain.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestMain {
+
+    @Test
+    public void testInputIsEven(){
+        assertTrue(MainClass.checkIfInputIsAnEvenNumber(22)); // Assertion
+    }
+}
